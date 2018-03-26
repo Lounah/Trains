e474c54ec7f12d09d0827185bcd9528435f0479e
Lounah <30981413+Lounah@users.noreply.github.com>
2018-03-26 18:44:53 +0300

    dagger init

--- app/src/main/java/com/example/lounah/trains/TrainsApplication.java
+++ app/src/main/java/com/example/lounah/trains/TrainsApplication.java
@@ -1,4 +1,40 @@
 package com.example.lounah.trains;
 
-public class TrainsApplication {
+import android.app.Application;
+
+import com.example.lounah.trains.di.DaggerAppComponent;
+import com.squareup.leakcanary.LeakCanary;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+import timber.log.Timber;
+
+public class TrainsApplication extends DaggerApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        installTimber();
+        installLeakCanary();
+
+    }
+
+    private void installTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+
+    private void installLeakCanary() {
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
+    }
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
 }
